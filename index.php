7514e0905b5298ef2f6c54fabd3a2912191728c6
Manon CAYETANO <92310939+manoncayetano@users.noreply.github.com>
2023-07-19 10:12:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Affichage du PDF</title>
+</head>
 <body>
-<?php include 'Portfolio-cayetano.pdf';?>
+    <h1>Affichage du PDF</h1>
+
+    <object data="Portfolio-cayetano.pdf" type="application/pdf" width="100%" height="600px">
+        <p>Votre navigateur ne prend pas en charge les PDF intégrés. Vous pouvez <a href="chemin/vers/votre-fichier.pdf">télécharger le fichier ici</a> à la place.</p>
+    </object>
 </body>
 </html>
